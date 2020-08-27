e0fa58bea5683c69438db965165d78401867e3ed
yalitzab <yalitzabueno@gmail.com>
2020-08-26 22:21:16 -0700

    save task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,12 @@ var createFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+      }
+    formEl.reset();  
+
     // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
