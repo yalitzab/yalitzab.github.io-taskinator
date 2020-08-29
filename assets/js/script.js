38c23d059a3a3db2ac24b56261c0013ddd88bd15
yalitzab <yalitzabueno@gmail.com>
2020-08-29 11:52:53 -0700

    add move based tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var pageContentEl = document.querySelector("#page-content");
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var createFormHandler = function(event) {
 
@@ -17,16 +19,65 @@ var createFormHandler = function(event) {
       }
     formEl.reset();  
 
-    // package up data as an object
-  var taskDataObj = {
-    name: taskNameInput,
-    type: taskTypeInput
-    
-};
+    var isEdit = formEl.hasAttribute("data-task-id");
+   
+    // has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput,taskTypeInput, taskId);
+    }      
+    else{
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput  
+        };
 
      // send it as an argument to createTaskEl
-  createTaskEl(taskDataObj);
+            createTaskEl(taskDataObj);
+        }
 }
+
+var taskStatusChangeHandler = function(event){
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+      // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    // console.log(taskName, taskType, taskId);
+    // find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    // changes button text back to normal
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+};
+
+
+
+
 
 var createTaskEl = function(taskDataObj) {
     //create list item
@@ -118,7 +169,7 @@ var taskButtonHandler = function(event) {
         var taskId = targetEl.getAttribute("data-task-id");
         deleteTask(taskId);
     }
-  };
+};
 
 //editing function
   var editTask = function(taskId) {
@@ -137,12 +188,13 @@ var taskButtonHandler = function(event) {
 
     formEl.setAttribute("data-task-id", taskId);
     
-    };
+};
 
 // Delete button function
   var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
-  };
+};
 
-pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("click", taskButtonHandler) ;
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
